b76572a0e75b83aeeced70bf94863d6237d19ac8
cemil özbay <cemil_ozbay@hotmail.com>
2022-01-24 23:33:57 +0300

    Add project files.

---
+++ Inheritance8523/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance8523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student()
+            {
+                Id = 1,
+                FirstName = "Çağıl",
+                LastName="Alsaç",
+                Department="Yazılım"
+            };
+            //Person studentPerson1 = new Person();
+            Person studentPerson2 = new Student();
+            studentPerson2.Id = 2;
+            studentPerson2.FirstName = "Leo";
+            studentPerson2.LastName = "Alsaç";
+
+            Person p = new Person();
+            p = new Student();
+            ((Student)p).Department = "Test";
+            (p as Student).Department = "Test";
+
+            // Polymorphism
+            Person customerPerson1 = new Customer() 
+            { 
+                Id=3,
+                FirstName="Ali",
+                LastName="Veli",
+                Address="Ankara"
+            };
+            Person studentPerson3 = new Student()
+            {
+                Id = 4,
+                FirstName="Can",
+                LastName="Kan",
+                Department="Doktor"
+            };
+
+            Person[] people = new Person[3]
+            {
+                new Person() // 0
+                {
+                    Id=1,
+                    FirstName="Çağıl",
+                    LastName="Alsaç"
+                },
+                new Student() //1
+                {
+                    Id=2,
+                    FirstName="X",
+                    LastName="Y",
+                    Department="Yazılım"
+                },
+                new Customer() // 2
+                {
+                    Id=3,
+                    FirstName="X",
+                    LastName="Y",
+                    Address="Eryaman Ankara"
+                }
+            };
+            foreach(Person person in people) 
+            {
+                Console.WriteLine("ID: " + person.Id + ", Adı: " + person.FirstName + ", Soyadı: " + person.LastName);
+            }
+            Customer customerInPeople = people[2] as Customer;
+            Console.WriteLine("ID: " + customerInPeople.Id + ", Adı: " + customerInPeople.FirstName + ", Soyadı: " + customerInPeople.LastName + ", Adres: " + customerInPeople.Address);
+
+            Ogrenci ogrenci = new Ogrenci() 
+            { 
+                Adi="Çağıl",
+                Soyadi="Alsaç",
+                Ulke=new Ulke() 
+                { 
+                    Id=1,
+                    Adi="Türkiye"
+                },
+                Sehri=new Sehir() 
+                { 
+                    Id=1,
+                    Adi="Ankara",
+                    UlkeId=1
+                }
+            };
+            Console.WriteLine($"Adı: {ogrenci.Adi}, Soyadı: {ogrenci.Soyadi}, Ülkesi: {ogrenci.Ulke.Adi}, Şehri: {ogrenci.Sehri.Adi}");
+
+            Console.ReadLine();
+        }
+    }
+    class Person // (Base , Parent Class , Concrete)
+    { 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Student : Person // (Sub , Child Class , Concrete)
+    { 
+        public string Department { get; set; }
+    }
+    class Customer : Person // C#'da sadece tek bir class üzerinden miras alınabilir.
+    { 
+        public string Address { get; set; }
+    }
+    class CustomerPayment : Customer // is a relationship
+    { 
+        public string CardNo { get; set; }
+    }
+    // Is a relationship:
+    // Has a relationship:
+    class Ogrenci 
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public Ulke Ulke { get; set; }
+        public Sehir Sehri { get; set; }
+    }
+    class Ulke
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+    }
+    class Sehir 
+    { 
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public int UlkeId { get; set; }
+    }
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db1 = new SqlDatabase() 
+            { 
+                Name="Microsoft SQL Server",
+                Company="Microsoft",
+                isRelational=true
+            };
+            db1.Create();
+            db1.Update();
+            
+            SqlDatabase db2= new SqlDatabase()
+            {
+                Name = "Microsoft SQL Server",
+                Company = "Microsoft",
+                isRelational = true
+            };
+            db2.Create();
+            db2.Update();
+
+            Database db3 = new OracleDatabase()
+            {
+                Name="Oracle",
+                Company="Oracle",
+                isRelational=true
+            };
+            db3.Read();
+            
+            Console.ReadLine();
+        }
+    }
+    class Database 
+    { 
+        public string Name { get; set; }
+        public string Company { get; set; } // property
+        
+        public bool isRelational; // field
+
+        public void Create()  // behavior
+        {
+            Console.WriteLine("Created by database.");
+        }
+        public virtual void Update()  // (Virtual)      
+        {                                                
+            Console.WriteLine("Updated by Database."); 
+        }
+        public virtual void Delete() // (Virtual)
+        {
+            Console.WriteLine("Deleted by Database.");
+        }
+        public virtual void Read() // (Virtual)
+        {
+            Console.WriteLine("Read by Database");
+        }
+    }
+    class SqlDatabase :Database
+    {
+        public override void Update() // (Override)
+        {
+            //base.Update();
+            Console.WriteLine("Updated by Sql.");
+        }
+    }
+    class OracleDatabase : Database 
+    {
+        public override void Read() // (Override)
+        {
+            base.Read();
+            Console.WriteLine("Read by Oracle.");
+        }
+    }
+}
